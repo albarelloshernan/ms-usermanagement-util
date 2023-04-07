14e5707ab83a0fa89a93d50e2fe8914b2698787e
albarellos.h.a@outlook.com <albarellos.h.a@outlook.com>
2023-04-07 11:42:30 -0300

    simple refactor

--- src/main/java/com/microservice/usermanagement/exception/CustomException.java
+++ src/main/java/com/microservice/usermanagement/exception/CustomException.java
@@ -4,22 +4,22 @@ import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
-public class CustomException extends RuntimeException{
+public class CustomException extends RuntimeException {
     private static final long serialVersionUID = 1L;
     private LocalDateTime timestamp;
-    private final String detail;
-    private final HttpStatus codigo;
+    private final String message;
+    private final HttpStatus httpStatus;
 
     public CustomException(String message, HttpStatus httpStatus) {
         this.timestamp = timestamp;
-        this.detail = message;
-        this.codigo = httpStatus;
+        this.message = message;
+        this.httpStatus = httpStatus;
     }
     @Override
     public String getMessage() {
-        return detail;
+        return message;
     }
     public HttpStatus getHttpStatus() {
-        return codigo;
+        return httpStatus;
     }
 }
