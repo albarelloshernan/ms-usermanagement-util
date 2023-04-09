b1b7d4b17802484d86274f5af2a12bf65bcb690b
albarellos.h.a@outlook.com <albarellos.h.a@outlook.com>
2023-04-09 11:54:25 -0300

    service spock tests

--- src/test/groovy/com/microservice/usermanagement/service/impl/AccountServiceImplTest.groovy
+++ src/test/groovy/com/microservice/usermanagement/service/impl/AccountServiceImplTest.groovy
@@ -1,10 +1,7 @@
 package com.microservice.usermanagement.service.impl
 
 import com.microservice.usermanagement.UserManagementApplication
-import com.microservice.usermanagement.converter.AccountRespEntityConverter
 import com.microservice.usermanagement.dto.req.AccountReqDto
-import com.microservice.usermanagement.dto.resp.AccountLoginRespDto
-import com.microservice.usermanagement.dto.resp.AccountRespDto
 import com.microservice.usermanagement.repository.AccountRepository
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpStatus
@@ -21,49 +18,57 @@ class AccountServiceImplTest extends Specification {
 
     AccountServiceImpl accountServiceImplMock
     AccountRepository accountRepositoryMock
-    AccountRespEntityConverter accountRespEntityConverterMock
 
     def setup() {
         accountServiceImplMock = Mock(AccountServiceImpl)
         accountRepositoryMock = Mock(AccountRepository)
-        accountRespEntityConverterMock = Mock(AccountRespEntityConverter)
     }
 
     @Unroll
-    def "SignUp"() {
+    def "Service SignUp returns #statusCode and #reasonPhrase on response"() {
         given:
-        def req = new AccountReqDto()
-        def resp = new AccountRespDto()
-        def httpStatus = Mock(HttpStatus)
+        AccountReqDto request = Mock()
+        ResponseEntity<Object> responseEntity = Mock() {
+            getStatusCode() >> httpStatus
+            getBody() >> reasonPhrase
+        }
 
         when:
-        def response = accountServiceImplMock.signUp(req)
+        def response = accountServiceImplMock.signUp(request)
 
         then:
-        response == expected
+        response == responseEntity
 
         where:
-        expected << new ResponseEntity(resp,httpStatus)
+        httpStatus                          | statusCode    |   reasonPhrase
+        HttpStatus.OK                       | 200           |   "OK"
+        HttpStatus.NOT_FOUND                | 404           |   "User not found."
+        HttpStatus.INTERNAL_SERVER_ERROR    | 500           |   "Error al insertar datos."
     }
 
     @Unroll
-    def "LogIn"() {
+    def "Service LogIn returns #statusCode and #reasonPhrase on response"() {
         given:
-        def resp = new AccountLoginRespDto()
-        def httpStatus = Mock(HttpStatus)
         def token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwcm9iYWRvckBnbWFpbC5jb20iLCJwYXNzd29yZCI6ImEyYXNmR2ZkZmRmN" +
                 "CIsImlhdCI6MTY4MDk5OTY3MSwiZXhwIjoxNjgxMDAzMjcxfQ.4jFatDRd81mV7PmS_rVoBlQ1Upslx4V90w1kUT13Oa4"
         def username = "probador@gmail.com"
         def password = "a2asfGfdfdf4"
+        ResponseEntity<Object> responseEntity = Mock() {
+            getStatusCode() >> httpStatus
+            getBody() >> reasonPhrase
+        }
 
         when:
         def response = accountServiceImplMock.logIn(token,username,password)
 
         then:
-        response == expected
+        response == responseEntity
 
         where:
-        expected << new ResponseEntity(resp,httpStatus)
+        httpStatus                          | statusCode    |   reasonPhrase
+        HttpStatus.OK                       | 200           |   "OK"
+        HttpStatus.NOT_FOUND                | 404           |   "User not found."
+        HttpStatus.UNPROCESSABLE_ENTITY     | 422           |   "Wrong username or password."
 
     }
 }
